7e1cda5d04e0c7c8f29832864d42f58624fe565f
VazhaK14 <khayrivazhasocial@gmail.com>
2026-03-29 00:35:57 +0700

    fix: prisma.js import formatter for server

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "./prisma";
+import { prisma } from "./prisma.js";
 import { openAPI } from "better-auth/plugins";
 
 export const auth = betterAuth({
